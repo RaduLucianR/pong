3e5b36cd17ff170cfd08b4c75ca51786973f5f16
RaduLucianR <radlucradulescu@gmail.com>
2024-10-27 23:46:08 +0100

    initial init with cleanup

---
+++ install_dep.sh
@@ -0,0 +1,10 @@
+sudo apt install \
+    libxrandr-dev \
+    libxcursor-dev \
+    libudev-dev \
+    libfreetype-dev \
+    libopenal-dev \
+    libflac-dev \
+    libvorbis-dev \
+    libgl1-mesa-dev \
+    libegl1-mesa-dev
